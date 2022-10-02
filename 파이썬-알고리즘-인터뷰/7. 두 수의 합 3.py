09209d6251341fe44cfd46d734f58e831fa264e8
obzva <dngyng1000@gmail.com>
2022-10-02 20:58:35 +0900

    change

--- 파이썬-알고리즘-인터뷰/7. 두 수의 합 3.py
+++ 파이썬-알고리즘-인터뷰/7. 두 수의 합 3.py
@@ -14,4 +14,6 @@ class Solution:
         for i, num in enumerate(nums):
             complement = target - num
             if complement in nums_map and i != nums_map[complement]:
+                # 'i != nums_map[complement]'가 없다면 해의 고유성을 보장할 수 없음
+                # ex: [3, 2, 4] , 6
                 return [i, nums_map[complement]]
