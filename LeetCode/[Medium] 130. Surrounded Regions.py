c1c88f26eead8b971ea5b5ccc1ccb63acf2d7aec
obzva <dngyng1000@gmail.com>
2022-10-21 00:03:17 +0900

    LeetCode

--- LeetCode/[Medium] 130. Surrounded Regions.py
+++ LeetCode/[Medium] 130. Surrounded Regions.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import *
 
 
@@ -6,21 +7,47 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        # m, n = len(board), len(board[0])
+        #
+        # def func(row, col):
+        #     if not (0 <= row < m and 0 <= col < n):
+        #         return True
+        #     if board[row][col] != 'O':
+        #         return False
+        #     board[row][col] = 'X'
+        #     is_surrounded = not func(row, col + 1) and not func(row + 1, col) and not func(row, col - 1) and not func(
+        #         row - 1, col)
+        #     if not is_surrounded:
+        #         board[row][col] = 'O'
+        #     return not is_surrounded
+        #
+        # for i in range(m):
+        #     for j in range(n):
+        #         func(i, j)
+
+        # DFS
+        if not board or not board[0]:
+            return
+
         m, n = len(board), len(board[0])
 
-        def func(row, col):
+        def dfs(row, col):
             if not (0 <= row < m and 0 <= col < n):
-                return True
+                return
             if board[row][col] != 'O':
-                return False
-            board[row][col] = 'X'
-            is_surrounded = not func(row, col + 1) and not func(row + 1, col) and not func(row, col - 1) and not func(
-                row - 1, col)
-            if not is_surrounded:
-                board[row][col] = 'O'
-            return not is_surrounded
+                return
+            board[row][col] = 'E'
+            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+            for dr, dc in directions:
+                dfs(row + dr, col + dc)
 
-        for i in range(m):
-            for j in range(n):
-                func(i, j)
+        borders = list(itertools.product(range(m), [0, n - 1])) + list(itertools.product([0, m - 1], range(1, n - 1)))
+        for row, col in borders:
+            dfs(row, col)
 
+        for row in range(m):
+            for col in range(n):
+                if board[row][col] == 'O':
+                    board[row][col] = 'X'
+                elif board[row][col] == 'E':
+                    board[row][col] = 'O'
